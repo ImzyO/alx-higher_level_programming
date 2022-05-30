3a4d72d60c5e33c681c722d622267fceae062772
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-05-30 07:35:38 -0700

    changed the values

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[109:111] + str[:6]
+str = str[40:67] + str[110:113] + str[:6]
 print(str)
