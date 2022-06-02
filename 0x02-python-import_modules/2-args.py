dfe72d19e807030ffb475788e82943cde90cce3f
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-02 06:51:08 -0700

    number pf list arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from sys import argv
     count = len(argv)
     if count != 0:
-        print("{}", "{}".format(len(argv), argument))
+        print("{}", "{}".format(count), argument))
     elif count == 0:
         print("{}", "{}".format(arguments: , .))
     else:
-        print("{}", "{}", "{]".format(len(argv), :, argv)
+        print("{}", "{}", "{]".format(count, :, argv)
