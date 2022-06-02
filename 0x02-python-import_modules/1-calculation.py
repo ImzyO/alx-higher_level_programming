e94159de049384f040a01f90b4e46d346ee6c012
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-02 04:17:14 -0700

    imports functions from the file, does some Maths

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__"
-    from calculator_1 import math:
+    from calculator_1 import add, sub, div, mul:
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".fomat(a, b, math.sqrt(a, b)))
+    print("{:d} - {:d} = {:d}".fomat(a, b, sub(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
