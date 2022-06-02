750c0316c5a98ff93d884c3b83bb6ee4b36b4c80
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-02 06:49:47 -0700

    prints number of list and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
     count = len(argv)
-    if argv != 0:
+    if count != 0:
         print("{}", "{}".format(len(argv), argument))
-    elif argv == 0:
+    elif count == 0:
         print("{}", "{}".format(arguments: , .))
     else:
         print("{}", "{}", "{]".format(len(argv), :, argv)
