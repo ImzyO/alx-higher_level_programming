5ad30121c44f8b1cea61edd9ef3f44942c816149
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-03 01:20:56 -0700

    summation of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     from sys import argv
-    add = int([1, 2, 3])
-    print("{} + {} + {} = {}".format(add, 1, 2, 3))
+    sum = 0
+    for i in argv[0:]
+        sum = sum + int(i)
+    print("{:d}".format(add))
