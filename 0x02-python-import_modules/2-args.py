0d57a38d5fc0df2c0b9134228057910af90277c9
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-06 11:40:12 -0700

    prints number of and lisst of arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     count = len(argv)
-    if count == 1:
-        print("{}", "{}".format(count, "argument"))
-    elif count != 1:
-        print("{:s}".format("arguments"))
-    else:
-        print("{}" or "{}".format(":", "."))
+     print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments", "." if l == 1 else ":"))
+     for i, s in enumerate(argv):
+         if i > 0:
+             print("{:d}: {:s}".format(i, s))
