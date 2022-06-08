757c9cc29db27ab6b9a53d68f087ec78ed0ec946
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-08 07:42:24 -0700

    multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    urn {key: value * 2 for key, value in a_dictionary.items()}
+    new = {}
+     for i in a_dictionary:
+         mul = 2 * a_dictionary[i]
+           new[i] = mul
+     return new
