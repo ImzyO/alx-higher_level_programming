2cc13c6853e0e321543f8bf679737352dc8f0c5c
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-09 04:06:20 -0700

    element occurence replaced

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,3 @@
 #!/usr/python3
 def search_replace(my_list, search, replace):
-     new_list = []
-     i = 1
-     for h in my_list:
-         if i== search:
-              new_list.insert(search, replace)
-         else:
-               new_list.append(h)
-               i += 1
-     return new_list
+    return [replace if  search == i else i for i in my_list]
