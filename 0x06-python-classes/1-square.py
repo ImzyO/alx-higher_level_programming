b1341f3d5d046e1cd2609e87f200847575620130
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-21 08:31:56 -0700

    defines a square based on file 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """class square that defines Square"""
+    def __init__(self, square_size):
+        """module __init__ for square_size, intiliazes"""
+        self.__size = square_size
