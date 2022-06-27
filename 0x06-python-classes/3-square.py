7e677a45291139504bff449d87062808320b8772
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-27 04:32:16 -0700

    class square, defines square based on file 2

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,15 @@
 #1/usr/bin/python3
+"""
+square module - prints square
+"""
 class Square:
-    """class square that defines square based on file 2"""
+    """
+    class square that defines square based on file 2
+    """
     def __init__(self, size=0):
-        """module __init__ for size square intialization"""
+        """
+        module __init__ for size square intialization
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -11,5 +18,7 @@ class Square:
             self.__size = size
 
     def area(self):
-        """current square area"""
+        """
+        current square area
+        """
         return(self.__size ** 2)
