88969859ddb26abcecf8d248db7fb2e4918c994f
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-27 04:19:46 -0700

    empty class, defines square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,6 +3,10 @@
 square module - printing squares
 """
 class Square:
-    """defined square with an empty class named Square"""
+    """
+    defined square with an empty class named Square
+    """
     pass
-    """pass used when there are no arguments or returns"""
+    """
+    pass used when there are no arguments or returns
+    """
