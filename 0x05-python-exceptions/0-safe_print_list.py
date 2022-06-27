1088adabb4c415821de4e4c67360528fc5ea0df5
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-27 05:57:06 -0700

    safe lis printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for l in range(0, x):
             print("{}".format(my_list[i]), end="")
             k += 1
-        print("")
+        print()
         return x
     except IndexError:
-        print("")
+        print()
         return k
