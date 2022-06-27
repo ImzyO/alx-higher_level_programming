e91b8c34cc719575a7b0d80d63e6b1e8ac18b366
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-27 04:46:26 -0700

    access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+square module - prints squares
+"""
+class Square:
+    """
+    class square now has getters and setters for size
+    """
+    def __init__(self, size=0):
+        """
+        class square take square takes in an int size
+        """
+        #methods inherited from the class
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        size getter returns the private size
+        """
+        #return current area
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the size
+        """
+        #has to be an int value
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        #returns area of square
+        return (self.__size ** 2)
+
