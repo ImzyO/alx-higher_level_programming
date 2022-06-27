cc252b9e488b226690fa26a3d7602858acfa539b
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-27 04:24:12 -0700

    defines square based on file 0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+square module- prints square
+"""
 class Square:
-    """class square that defines Square"""
+    """
+    class square that defines Square
+    """
     def __init__(self, square_size):
-        """module __init__ for square_size, intiliazes"""
+        """
+        module __init__ for square_size, intiliazes
+        """
         self.__size = square_size
