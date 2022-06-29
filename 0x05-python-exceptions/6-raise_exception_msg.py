c70b0201e33319b3a6f5d18d4663c5d9e3584d88
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-06-29 11:43:33 -0700

    raised name exception

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,3 @@
-#1/usr/bin/python3
-def raise_exception_msg(message="Name is not recognized"):
-    raise NameError(message="Name is not recognized")
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError(message="")
