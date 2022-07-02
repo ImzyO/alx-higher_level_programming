7ddb28066ba6d5b8e085db2e5ff1afba4569c5bb
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-07-01 17:49:00 -0700

    square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,8 +6,8 @@ class Square:
     """
     class square that defines Square
     """
-    def __init__(self, square_size):
+    def __init__(self, size):
         """
         module __init__ for square_size, intiliazes
         """
-        self.__size = square_size
+        self.__size = size
