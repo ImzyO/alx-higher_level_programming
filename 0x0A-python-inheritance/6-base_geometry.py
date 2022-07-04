0991c97b0645afee6adde793fffe88ee7f69f337
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-07-04 06:21:25 -0700

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module with public instance method basegeometry"""
+class BaseGeometry:
+    """class basegeometry, based on file 5"""
+    def area(self):
+        """method for area claculation"""
+        raise Exception("area() is not implemented")
