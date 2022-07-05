de61c96f43350392b2a82c744b0e743e83514ca3
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-07-05 07:05:59 -0700

    to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for to JSON string"""
+import JSON
+#import module json
+def to_json_string(my_obj):
+    """a function that returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
