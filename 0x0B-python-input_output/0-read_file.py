7e964bd9191830fac8aaf206303f3976dd00dc41
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-07-05 06:43:46 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""this is a module for read file"""
 def read_file(filename=""):
+    """ a function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, "r", encoding="UTF-8") as F:
         print(F.read(), end="")
