9c3ea5be51462289d8df6931a3770bd98ec786d2
Imani <100032916+ImzyO@users.noreply.github.com>
2022-10-05 14:36:41 +0300

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,20 +25,36 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """updates class Square, adds this public method"""
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-        elif len(args) != 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[2]
-                self.y = args[3]
-            except IndexError;
-            pass
-        else:
-            print()
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for j, k in kwargs.items():
+                if j == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = k
+                elif j == "size":
+                    self.size = k
+                elif j == "x":
+                    self.x = k
+                elif j == "y":
+                    self.y = k
 
     def to_dictionary(self):
         """method returns dictionary representation"""
-        return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
+        return {'id': self.id, 'x': self.x, 'y': self.y, 'size': self.width}
