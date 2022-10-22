98c1a7a45a801f37869ea6677d56cd0aacd19143
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-10-22 04:08:16 -0700

    removed whitespace

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,5 @@ from sys import argv
 
 if __name__ == '__main__':
     req = request.Request(argv[1])
-    
     with request.urlopen(req) as response:
         print(dict(response.headers).get('X-Request-Id'))
