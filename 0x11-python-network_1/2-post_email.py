b01587905cd02a1080f8ccc488bade1d49785ad2
Imani <100032916+ImzyO@users.noreply.github.com>
2022-10-22 14:14:49 +0300

    git pull

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,13 +7,16 @@ import urllib.request
 from sys import argv
 import urllib.parse
 
+import sys
+import urllib.parse
+import urllib.request
+
+
 if __name__ == "__main__":
-        url = argv[1]
-        data = {
-        'email': argv[2]
-        }
-        email_dat = urllib.parse.urlencode(data).encode("ascii")
-        req = urllib.request.Request(url, data)
-        with urllib.request.urlopen(req) as response:
-                page = response.read()
-                print(page.decode("utf-8"))
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
