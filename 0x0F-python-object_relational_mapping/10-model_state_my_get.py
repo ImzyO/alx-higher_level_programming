cfe787f09643b0f85676bccf1b711377599cb0ad
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-10-24 03:35:06 -0700

    debugged

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -6,7 +6,7 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(./
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(\
             argv[1], argv[2], argv[3])
 
     engine = create_engine(db_uri)
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,13 +3,12 @@
 import MySQLdb
 from sys import argv
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,./
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,\
             passwd=argv[2], db=argv[3])
     with db.cursor() as typed:
-        typed.execute("""SELECT cities.id, cities.name FROM JOIN states ON cities.state_id = states.id\
-                states.name LIKE BINARY %(state_name)s ORDER BY cities.id ASC""", {'state_name': argv[4]})
+        typed.execute("""SELECT cities.id, cities.name FROM JOIN states ON cities.state_id = states.id states.name LIKE BINARY %(state_name)s ORDER BY cities.id ASC""", {'state_name': argv[4]})
 
-        rows = cur.fetchall()
+        rows = typed.fetchall()
 
     if rows is not None:
         print(", ".join([row[1] for row in rows]))
