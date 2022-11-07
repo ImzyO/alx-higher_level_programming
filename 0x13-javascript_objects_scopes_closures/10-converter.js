75b6165948f367ef4baee5845bb4f5764bea5a51
Imani <100032916+ImzyO@users.noreply.github.com>
2022-11-07 21:54:06 +0300

    converter

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// function that converts a number from base 10 to another base passed as argument:
+// function that converts a number from base 10 to another base passed as argument: return converter
 exports.converter = function (base) {
 	function converter (num) {
 		return num.toString(base);
-	}
-	return converter
+	};
 };
