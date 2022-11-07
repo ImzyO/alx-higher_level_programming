ed56f5e194b15dfe512c2acb3748aae43cb5072d
Imani <100032916+ImzyO@users.noreply.github.com>
2022-11-07 21:46:32 +0300

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
 //class Square Defines a square inherits from Square of 5-square.js:
-class Square extends Square {
-	super(name, size);
+const square = require('./5-square');
 
-	charPrint(c) {
-		if (c) {
-			for (let count = 0; count < this.height; count++) {
-				console.log('c'.repeat(this.width));
-			}
-		} else if (c === undefined) {
-			for (count2 = 0; count2 < this.height; count2++) {
-				console.log('X'.repeat(this.width));
-			}
-		}
-	}
+class Square extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+    } else {
+      console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
+    }
+  }
 }
 module.exports = Square;
