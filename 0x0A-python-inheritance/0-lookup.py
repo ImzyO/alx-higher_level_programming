52ec85ce30c9948fa5bc80b5e538e898b1d56a29
Imani <100032916+ImzyO@users.noreply.github.com>
2022-11-08 15:04:47 +0300

    whitespace

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module that has the method lookup"""
+
+
 def lookup(obj):
     """a function that returns the list of availableattributes and methods of an object"""
     return dir(obj)
