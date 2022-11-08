1dfcec224cd791ec523073d872ced95bd56608c6
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-11-08 03:40:18 -0800

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """module that has the method lookup"""
 def lookup(obj):
-    """
-    a function that returns the list of available
-    attributes and methods of an object
-    """
+    """a function that returns the list of availableattributes and methods of an object"""
     return dir(obj)
