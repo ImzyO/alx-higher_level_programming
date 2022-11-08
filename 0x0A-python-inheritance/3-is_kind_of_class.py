1e36099fe5e91e72e3492ba8ba7333ff48a1cf7c
Imani <100032916+ImzyO@users.noreply.github.com>
2022-11-08 15:34:44 +0300

    3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module of same class or inherit from specified class"""
+
+
 def is_kind_of_class(obj, a_class):
     """method function is_kind_of_class with attribute a_class"""
     # Returns True if obj is an instance of
